43ef6ae7fc8c81ba74789c966b4e66e8564d9c78
sachux <sachinkalpetta@gmail.com>
2022-06-08 23:50:42 +0530

    Updated router

--- router/generatorRouter.js
+++ router/generatorRouter.js
@@ -1,7 +1,17 @@
 import express from "express";
+import ReportService from "../service/ReportService.js";
 
 const router = express.Router();
 
-router.get('/report', function(req, res) { res.status(200).send({"something":true});});
+router.get('/report', function(req, res) { 
+    const reportService = new ReportService();
+    const data = {
+        "projectName" : "Road Widening",
+        "announcementDate" : "2022-02-05",
+        "sponsor" : "Goverment of India",
+        "location" : "WhiteField, Bangalore"
+    }
+    res.status(200).send(reportService.createReport(data, "ReportTemplate.ejs"));
+});
 
 export default router;
