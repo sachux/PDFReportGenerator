584f7d076ed3e6b1cff770f5aeabfef2fed69ce2
sachux <sachinkalpetta@gmail.com>
2022-06-12 00:01:48 +0530

    Added data property in the sample data

--- router/generatorRouter.js
+++ router/generatorRouter.js
@@ -3,15 +3,17 @@ import ReportService from "../service/ReportService.js";
 
 const router = express.Router();
 
-router.get('/report', function(req, res) { 
+router.get('/report', async function(req, res) { 
     const reportService = new ReportService();
     const data = {
+        data :  {
         "projectName" : "Road Widening",
         "announcementDate" : "2022-02-05",
         "sponsor" : "Goverment of India",
         "location" : "WhiteField, Bangalore"
-    }
-    res.status(200).send(reportService.createReport(data, "ReportTemplate.ejs"));
+    }};
+    const pdf = await reportService.createReport(data, "ReportTemplate.ejs");
+    res.status(200).send(pdf);
 });
 
 export default router;
